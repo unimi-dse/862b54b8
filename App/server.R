a530ae65a3a63ef84367330328d968f03c371028
AggiudicatoIrene <60004478+AggiudicatoIrene@users.noreply.github.com>
2020-02-08 13:37:31 +0100

    Add files via upload

---
+++ App/server.R
@@ -0,0 +1,21 @@
+
+server <-function(input, output){
+    data <- read.csv(system.file ("extdata", "Provadue.csv", package = "midterm") sep = ";"
+
+    output$output <- renderText(paste(input$button))
+
+    output$plot <- renderPlotly({
+
+        id <- as.integer(input$button)
+
+        plot <- plot_ly(x = data$Country.Name, y = data[,id],
+                       type = "bar", marker = list(colour = "rgb(49,130,189)")
+
+
+
+        )
+
+
+    })
+
+}
