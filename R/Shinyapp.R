30161cf856cc012bd8bfbb8d0bd7ef6522e3af7b
AggiudicatoIrene <60004478+AggiudicatoIrene@users.noreply.github.com>
2020-02-08 15:22:55 +0100

    Add files via upload

---
+++ R/Shinyapp.R
@@ -0,0 +1,12 @@
+#' Shiny app ue an interactive interface to show the expenditure in education in Italy and its closest countries.
+#'
+#'The function goes without arguments
+#'
+#'
+#' @return runs Shiny app.
+#'
+#' @export
+
+Eduexp <- function (){
+  shiny::runApp(system.file("App", package = "midterm"), launch.browser = T)
+}
