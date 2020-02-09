94e0b26652541e01969553c99aad42bdec3e85b3
AggiudicatoIrene <60004478+AggiudicatoIrene@users.noreply.github.com>
2020-02-09 17:07:27 +0100

    Add files via upload

---
+++ App/ui.R
@@ -0,0 +1,21 @@
+shiny::shinyUI (
+  fluidPage(
+    titlePanel("Expenditure for education in Italy and its neighbours"),
+    sidebarLayout(
+      sidebarPanel(
+        radioButtons("button", "Select year:",
+                     c("2014" = 2,
+                       "2015" = 3,
+                       "2016" = 4))
+
+      ),
+      mainPanel(
+        textOutput("output"),
+        plotly::plotlyOutput("plot")
+      )
+
+
+    )
+  )
+)
+
---
+++ R/Shinyapp.R
@@ -0,0 +1,3 @@
+Expenditure <- function (){
+  shiny::runApp(system.file("App", package = "midterm"), launch.browser = T)
+}
