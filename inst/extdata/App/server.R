42e40390b37e932d89296c29150311540fdb0e6e
AggiudicatoIrene <60004478+AggiudicatoIrene@users.noreply.github.com>
2020-02-11 11:37:13 +0100

    Add files via upload

---
+++ inst/extdata/App/server.R
@@ -0,0 +1,19 @@
+shiny::shinyServer(function(input, output){
+  data <- read.csv(system.file("extdata", "Provadue.csv", package = "Education"), sep = ";")
+
+  output$output <- shiny::renderText(paste(input$button))
+
+  output$plot <- plotly::renderPlotly({
+
+    id <- as.integer(input$button)
+
+    plot <- plotly::plot_ly(x = data$Country.Name, y = data[,id],
+                            type = "bar", marker = list(colour = "rgb(49,130,189)")
+
+
+    )
+
+
+  })
+
+})
