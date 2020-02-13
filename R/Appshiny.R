aa8a400b45a41b964aeff594dad16cf879cfa3fc
AggiudicatoIrene <irene.aggiudicato@studenti.unimi.it>
2020-02-13 09:39:11 +0100

    updated roxygen

--- R/Appshiny.R
+++ R/Appshiny.R
@@ -4,7 +4,7 @@
 #'This function permits to the user to launch the shiny app.
 #'Through this app, there will be printed out three different graphs, depending on the choice of the user. User, in fact, can choose three different years and according to them, shiny will run a different plot for the countries in consideration.
 #'@return shiny app
-#'
+#'@example ExpEdu()
 #'
 #'@export
 
--- R/Getdata.R
+++ R/Getdata.R
@@ -2,6 +2,7 @@
 #'
 #'@description this function permits the users to get the data from a csv file stored in the folder "inst/extdata".
 #'Putting this function into the shiny app, it will get the data needed for the package.
+#'@example getData()
 #'
 #'@export
 
